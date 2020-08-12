98d802af49672d889a4952992bd406ca3846f6c6
John Adler <john.adler@protonmail.ch>
2020-08-12 10:02:21 -0400

    Use solc 0.7.0.

--- truffle-config.js
+++ truffle-config.js
@@ -96,8 +96,8 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default:
-      // truffle's version)
+      // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.7.0",
       // docker: true,        // Use "0.5.1" you've installed locally with
       // docker (default: false)
       // settings: {          // See the solidity docs for advice about

2c6fb81ca84081e8169a6765d64d24f741aa22d4
John Adler <john.adler@protonmail.ch>
2020-08-14 12:15:07 -0400

    Add test for constructor parameters.

--- test/Tendermint-ORU.js
+++ test/Tendermint-ORU.js
@@ -2,6 +2,19 @@ const Tendermint_ORU = artifacts.require("Tendermint_ORU");
 
 contract("Tendermint_ORU", async (accounts) => {
   it("should deploy", async () => {
-    let instance = await Tendermint_ORU.deployed();
+    const instance = await Tendermint_ORU.deployed();
+  });
+
+  it("should set constructor parameters", async () => {
+    const instance = await Tendermint_ORU.deployed();
+
+    const genesisHash = await instance._genesisHash.call();
+    assert.equal(genesisHash, "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470");
+    const genesisSubmissionHash = await instance._genesisSubmissionHash.call();
+    assert.equal(genesisSubmissionHash, "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470");
+    const bondSize = await instance._bondSize.call();
+    assert.equal(bondSize, 0);
+    const fraudTimeout = await instance._fraudTimeout.call();
+    assert.equal(fraudTimeout, 0);
   });
 });
